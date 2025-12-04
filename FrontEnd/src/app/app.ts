c469e2a983e1e9af1ab18c18899d8e20e598cd50
Joshefs Rubio <rc202531676@alm.buap.mx>
2025-12-04 14:05:08 -0600

    Add responsive mobile menu and hero background effects
    Implemented a mobile navigation menu with animated hamburger button and smooth transitions in app.html and app.ts. Enhanced the hero section with background lights and grid effects for improved visual appeal. Also added z-index adjustments to ensure proper layering of content.

--- FrontEnd/src/app/app.ts
+++ FrontEnd/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, HostListener } from '@angular/core';
 import { Hero } from './hero/hero';
 import { ProjectsSection } from './projects-section/projects-section';
 import { ContactFooter } from './contact-footer/contact-footer';
@@ -15,5 +15,24 @@ import { ContactFooter } from './contact-footer/contact-footer';
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('FrontEnd');
+
+  mobileMenuOpen = signal<boolean>(false);
+
+  //Alterna el menú
+  toggleMobileMenu() {
+    this.mobileMenuOpen.update(v => !v);
+  }
+
+  //Cierra menú al clicar fuera del header
+  @HostListener('window:click', ['$event'])
+  closeMenuOnClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+
+    if (target.closest('header')) return;
+
+    // Cierra menú
+    this.mobileMenuOpen.set(false);
+  }
 }
