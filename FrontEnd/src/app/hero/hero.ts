84646925a72cb5e98f37161a571c7103b9a7276d
Joshefs Rubio <rc202531676@alm.buap.mx>
2025-12-04 13:10:06 -0600

    Remove SSR support and enhance hero section UI
    Removed Angular SSR configuration and related server files, simplifying the project to client-side only. Updated the hero section with animated titles, improved button and icon styles, and enhanced visual effects. Adjusted project card background color for better contrast.

--- FrontEnd/src/app/hero/hero.ts
+++ FrontEnd/src/app/hero/hero.ts
@@ -5,14 +5,26 @@ import * as AOS from 'aos';
   selector: 'app-hero',
   standalone: true,
   templateUrl: './hero.html',
-  styleUrl: './hero.css',
+  styleUrls: ['./hero.css'],
 })
-export class Hero implements AfterViewInit{
+export class Hero implements AfterViewInit {
+
   ngAfterViewInit() {
+    // Inicializar AOS
     AOS.init({
       duration: 1000,
       easing: 'ease',
       once: true,
     });
+
+    // Animación de palabras
+    const words = ["Full Stack", "Front End", "Back End", "Cloud"];
+    let currentIndex = 0;
+    const animatedWord = document.getElementById("animated-word") as HTMLElement;
+
+    setInterval(() => {
+      currentIndex = (currentIndex + 1) % words.length;
+      animatedWord.innerText = words[currentIndex];
+    }, 2000);
   }
 }
